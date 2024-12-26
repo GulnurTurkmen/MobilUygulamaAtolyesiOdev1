44e27097cc9562031e8bd44e93224bf03253aa7a
Gulnur Turkmen <trkmengulnur@gmail.com>
2024-12-26 22:23:05 +0300

    commit mesajı

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  int yas = 24;
+  String ad = "Gülnur";
+  dynamic soyad = "Türkmen";
+  bool yetiskinMi = true;
+
+  print("Adım: $ad");
+  print("Soyadım: $soyad");
+  print("Yaşım: $yas");
+  print("Yetişkin mi?: $yetiskinMi");
+
+  double double_sayi = 1.1;
+  String KarakterTanimi = "A";
+}
+
